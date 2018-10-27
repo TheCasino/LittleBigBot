d1621b852cb48caeb796656c885d929d78d30e17
littlebigtragedy <abyssal512@gmail.com>
2018-10-27 22:47:24 +1300

    First commit!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LittleBigBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

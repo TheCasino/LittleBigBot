60bfaf89b6c7a3bd731289e3bc157c8adbb937d3
littlebigtragedy <abyssal512@gmail.com>
2018-10-29 17:49:41 +1300

    Explain what the default value description attribute does!

--- Attributes/DefaultValueSummaryAttribute.cs
+++ Attributes/DefaultValueSummaryAttribute.cs
@@ -2,9 +2,15 @@
 
 namespace LittleBigBot.Attributes
 {
+    /// <summary>
+    ///     Provides a resource string to the consumer detailing what the default value for a command parameter is.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Parameter)]
     public class DefaultValueDescriptionAttribute : Attribute
     {
+        /// <summary>
+        ///     Provides a resource string to the consumer detailing what the default value for a command parameter is.
+        /// </summary>
         public DefaultValueDescriptionAttribute(string defaultValueDescription)
         {
             DefaultValueDescription = defaultValueDescription;
